64995df6c6c03fd027d2632d8cec1301a86bb802
Daniel Cornelius <danielcorneliusgonzalez@gmail.com>
2026-06-12 02:27:14 -0300

    Add: testes de exibicao do iteratir na main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "LivroAcao.h"
 #include "LivroFiccao.h"
 #include "LivroTipoFactory.h"
+#include "Colecao.h"
 
 int main()
 {
@@ -12,33 +13,29 @@ int main()
     LivroTipo *tipoFiccao = factory.getTipo("Ficcao");
 
     Livro *livroAcao1 = new LivroAcao("Autor 1", "Titulo 1", tipoAcao);
-
     Livro *livroAcao2 = livroAcao1->clonar();
 
-    cout << "Livro de Acao original:\n";
-    livroAcao1->getLivro();
-    livroAcao1->mostrarFlyweightId();
-    cout << "Endereco do livro acao original: " << livroAcao1<< '\n';
-
-    cout << "\nLivro de Acao clonado:\n";
-    livroAcao2->getLivro();
-    livroAcao2->mostrarFlyweightId();
-    cout << "Endereco do livro acao clone: "<< livroAcao2 << '\n';
-
     Livro *livroFiccao1 = new LivroFiccao("Autor 2", "Titulo 2", tipoFiccao);
-
     Livro *livroFiccao2 = livroFiccao1->clonar();
 
-    cout << "\nLivro de Ficcao original:\n";
-    livroFiccao1->getLivro();
-    livroFiccao1->mostrarFlyweightId();
-    cout << "Endereco do livro Ficcao original: " << livroFiccao1 << '\n';
+    Colecao acervo;
 
-    cout << "\nLivro de Ficcao clonado:\n";
-    livroFiccao2->getLivro();
-    livroFiccao2->mostrarFlyweightId();
-    cout << "Endereco do livro Ficcao clone: " << livroFiccao2 << '\n';
+    acervo.AdicionarLivro(livroAcao1);
+    acervo.AdicionarLivro(livroAcao2);
+    acervo.AdicionarLivro(livroFiccao1);
+    acervo.AdicionarLivro(livroFiccao2);
 
+    cout << "\n=== ACERVO GERAL ===\n";
+    Iterator* it = acervo.CriarIterator();
+    
+    while(it->hasNext()) {
+        Livro* livro = it->next();
+        livro->getLivro();
+        livro->mostrarFlyweightId();
+        cout << "Endereco do livro: " << livro << "\n" << endl;
+    }
+
+    delete it;
     delete livroAcao1;
     delete livroAcao2;
     delete livroFiccao1;
